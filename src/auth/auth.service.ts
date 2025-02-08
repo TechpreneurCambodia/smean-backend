0e517c5f6edd18601db95808580b1ec620b97676
lengkimtry <kimt47494@gmail.com>
2025-02-08 11:56:24 +0700

    Implement authentication features with sign-in and user retrieval endpoints; add JWT support and validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,32 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { UsersRepository } from "./users.repository";
 import { AuthCredentialsDto } from "./dto/auth-credentials";
+import { SignInDto } from "./dto/sign-in.dto";
+import * as bcrypt from "bcrypt";
+import { JwtService } from "@nestjs/jwt";
+import { JwtPayload } from "./jwt-payload-interface";
 
 @Injectable()
 export class AuthService {
-  constructor(private usersRepository: UsersRepository) {}
+  constructor(
+    private usersRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     // Call the repository method to create a user
     await this.usersRepository.createUser(authCredentialsDto);
+  }
+  async signIn(signInDto: SignInDto): Promise<{accessToken: string}> {
+    const { username, password } = signInDto;
+    const user = await this.usersRepository.findOneByUsername(username);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload: JwtPayload = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('Invalid credentials');
+    }
   }
 }
