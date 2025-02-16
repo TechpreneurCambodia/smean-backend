83304a7d610061e01d7efa1608d8db86a2794719
UnSreypich <unsreypich999@gmail.com>
2025-02-16 19:50:50 +0700

    Project and docker config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = new ConfigService();
+  // Enable CORS Cross Origin Resource Sharing
+  app.enableCors({
+    origin: configService.get('FRONTEND_URL'), 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('SMEAN API Documentation')
+    .setDescription(' API information')
+    .setVersion('1.0')
+    .addTag('money')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, documentFactory);
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
