09d2ee8e59a5bd9a3ecbb4c76f3724510a88ee93
UnSreypich <unsreypich999@gmail.com>
2025-02-19 13:16:04 +0700

    Add User and Audio modules with respective controllers to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,11 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AudioModule } from './audio/audio.module';
+import { UserController } from './user/user.controller';
+import { AuthController } from './auth/auth.controller';
+import { AudioController } from './audio/audio.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -27,8 +32,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       }),
       inject: [ConfigService],
     }),
+    
+    AudioModule,
+    UserModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, UserController, AuthController, AudioController],
   providers: [AppService],
 })
 export class AppModule {}
