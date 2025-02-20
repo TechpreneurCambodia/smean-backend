6246383cb77ab0f16e711a6e714ba177f8184bed
lengkimtry <kimt47494@gmail.com>
2025-02-20 14:11:02 +0700

    Enhance application setup with Swagger documentation and CORS support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as dotenv from "dotenv";
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS Cross Origin Resource Saharing
+  app.enableCors({
+    origin: 'http://localhost:3001', // Frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('SMEAN API Documentation')
+    .setDescription(' API information')
+    .setVersion('1.0')
+    .addTag('money')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, documentFactory);
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await process.env.REFRESH_SECRET}`);
 }
 bootstrap();
